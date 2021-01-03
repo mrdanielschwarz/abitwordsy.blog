ec7501ce1d10d956294f7bba6f4186af9e3285d9
mrdanielschwarz <76550599+mrdanielschwarz@users.noreply.github.com>
2021-01-03 19:18:09 +0800

    c

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,20 @@ module.exports = (function (eleventyConfig) {
         return moment(theDate).tz("GMT").format("Do MMMM YYYY");
     });
     
+    eleventyConfig.addFilter("starify", function (rating) {
+        
+        let output = [];
+        
+        for (var i = rating; i >= 1; i--) output.push('<i class="fas fa-star"></i>');
+        
+        if (i == .5) output.push('<i class="fas fa-star-half-alt"></i>');
+        
+        for (let i = (5 - rating); i >= 1; i--) output.push('<i class="far fa-star"</i>');
+        
+        return `${ rating } ${ output.join("") }`;
+        
+    });
+    
     eleventyConfig.setTemplateFormats(["html", "md", "jpg", "jpeg", "liquid"]);
     
     return {
